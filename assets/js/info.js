68cce6703cb186d393582b364395f5d3142d1d4a
clinton vilakati <vilakaticlinton@gmail.com>
2024-12-18 03:21:58 +0200

    fixed info controller

--- assets/js/info.js
+++ assets/js/info.js
@@ -8,9 +8,16 @@ if (window.getPageName() == "info") {
       document.getElementById('question-text').textContent = "You failed...you cannot continue beyond this stage";
     } else {
       document.getElementById('question-text').textContent = "Click the button below to continue to stage 2.";
+      document.getElementById('step_2').classList.toggle("d-none");
     }
   }
   else if (stage == "Puzzle") {
-    
+    document.getElementById('question-text').classList.toggle("d-none");
+    if (puzzleTotalScore < 25) {
+      document.getElementById('question-text').textContent = "You failed...you cannot continue beyond this stage";
+    } else {
+      document.getElementById('question-text').textContent = "Click the button below to continue to stage 3.";
+      document.getElementById('step_3').classList.toggle("d-none");
+    }
   }
 }
